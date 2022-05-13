fda03b3755a04a1b1fc167a50fe5e27c9f0ca0c7
DipinAZ <dipin@azinternet.io>
2022-05-13 23:30:45 +0530

    Update sfcalendar events

---
+++ lib/Model/event_model.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class EventModel {
+  final String title;
+  final DateTime from;
+  final DateTime to;
+  final Color backgroundColor;
+  final bool isAllDay;
+
+  const EventModel({
+    required this.title,
+    required this.from,
+    required this.to,
+    required this.backgroundColor,
+    required this.isAllDay,
+  });
+}
+
+
+//this.backgroundColor = Colors.lightGreen,
